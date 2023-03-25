84e733035760c1d084af36a65e448af3b39efee0
PhiL <philip_cleugh2@hotmail.com>
2023-03-25 12:53:58 +0000

    bonus skill mod increase

--- MMOCoreORB/bin/scripts/managers/skill_mod_manager.lua
+++ MMOCoreORB/bin/scripts/managers/skill_mod_manager.lua
@@ -54,7 +54,7 @@ skillModLimits = {
 	--{258, x, x}, -- SKILLBOX
 	
 	-- Bonus Mods Wearables / Structure
-	{4097, -25, 25},  --WEARABLE
+	{4097, -125, 125},  --WEARABLE
 	{4098, -125, 125}, -- STRUCTUREMOD
 	
 	-- Temp mods, not displayed (on timers or ability bonus)
@@ -63,9 +63,9 @@ skillModLimits = {
 }
 
 disabledWearableSkillMods = {
-	"combat_healing_ability",
-	"healing_ability",
+	--"combat_healing_ability",
+	--"healing_ability",
 	"keep_creature",
 	"stored_pets",
-	"combat_medic_effectiveness",
+	--"combat_medic_effectiveness",
 }
