f6877106a964e49262c86fbe0671e22569a8af60
PhiL <philip_cleugh2@hotmail.com>
2023-03-25 12:50:31 +0000

    skill mod always max

--- MMOCoreORB/src/server/zone/managers/loot/LootManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/loot/LootManagerImplementation.cpp
@@ -535,7 +535,7 @@ void LootManagerImplementation::setSkillMods(TangibleObject* object, const LootI
 			int max = (int) Math::max(-1.f, Math::min(25.f, (float) round(0.1f * level + 3)));
 			int min = (int) Math::max(-1.f, Math::min(25.f, (float) round(0.075f * level - 1)));
 
-			int mod = System::random(max - min) + min;
+			int mod = max;
 
 			if(mod == 0)
 				mod = 1;
