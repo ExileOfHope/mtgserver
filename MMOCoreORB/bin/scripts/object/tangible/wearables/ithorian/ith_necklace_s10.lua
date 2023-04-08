24e77c26d41aaecc27da16cf31bba1b876259cf3
PhiL <philip_cleugh2@hotmail.com>
2023-04-09 00:24:43 +0100

    Jewels with slots

--- MMOCoreORB/bin/scripts/object/tangible/wearables/ithorian/ith_necklace_s10.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/ithorian/ith_necklace_s10.lua
@@ -47,15 +47,15 @@ object_tangible_wearables_ithorian_ith_necklace_s10 = object_tangible_wearables_
 				"object/mobile/vendor/ithorian_female.iff",
 				"object/mobile/vendor/ithorian_male.iff" },
 
-	numberExperimentalProperties = {1, 1, 1},
-	experimentalProperties = {"XX", "XX", "XX"},
-	experimentalWeights = {1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null"},
-	experimentalSubGroupTitles = {"null", "null", "hitpoints"},
-	experimentalMin = {0, 0, 1000},
-	experimentalMax = {0, 0, 1000},
-	experimentalPrecision = {0, 0, 0},
-	experimentalCombineType = {0, 0, 4},
+	numberExperimentalProperties = {1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX", "XX"},
+	experimentalWeights = {1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null", "null"},
+	experimentalSubGroupTitles = {"null", "null", "sockets", "hitpoints"},
+	experimentalMin = {0, 0, 0, 1000},
+	experimentalMax = {0, 0, 0, 1000},
+	experimentalPrecision = {0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 4, 4},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_ithorian_ith_necklace_s10, "object/tangible/wearables/ithorian/ith_necklace_s10.iff")
