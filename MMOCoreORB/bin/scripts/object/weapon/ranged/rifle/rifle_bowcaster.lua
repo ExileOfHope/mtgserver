04c675c2a333049fc582feef914209d971ae106e
PhiL <philip_cleugh2@hotmail.com>
2023-04-17 20:22:24 +0100

    improved bowcaster

--- MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_bowcaster.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_bowcaster.lua
@@ -61,7 +61,7 @@ object_weapon_ranged_rifle_rifle_bowcaster = object_weapon_ranged_rifle_shared_r
 	xpType = "combat_rangedspecialize_rifle",
 
 	-- See http://www.ocdsoft.com/files/certifications.xls
-	certificationsRequired = {  },
+	certificationsRequired = { },
 
 	-- See http://www.ocdsoft.com/files/accuracy.xls
 	creatureAccuracyModifiers = { "rifle_accuracy" },
@@ -109,7 +109,7 @@ object_weapon_ranged_rifle_rifle_bowcaster = object_weapon_ranged_rifle_shared_r
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expEffeciency", "exp_durability", "null", "null", "null", "expRange", "expEffeciency", "expEffeciency", "expEffeciency"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "roundsused", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "attackhealthcost", "attackactioncost", "attackmindcost"},
 	experimentalMin = {0, 0, 80, 200, 8, 15, 30, 750, -70, -10, 50, 10, 39, 39, 39},
-	experimentalMax = {0, 0, 150, 375, 55, 29, 65, 1500, -70, 15, 50, 10, 21, 21, 21},
+	experimentalMax = {0, 0, 150, 375, 5.5, 29, 65, 1500, -70, 15, 50, 10, 21, 21, 21},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
